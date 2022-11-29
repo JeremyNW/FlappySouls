934828a2b7f2c16eefbcb9fd11ef8d7fb9fb1a18
Jeremy Warren <jeddynwarren@gmail.com>
2022-11-28 19:05:23 -0700

    Hero Updates

--- FlappySouls/Hero.swift
+++ FlappySouls/Hero.swift
@@ -11,17 +11,34 @@ import SpriteKit
 class Hero: SKSpriteNode, GameObject {
     
     var yVelocity = CGFloat(0)
+    var upWing: SKNode?
+    var flapWing: SKNode?
     
     func setUp(with controller: GameController) {
-        
+        upWing = childNode(withName: "SKWingUp")
+        flapWing = childNode(withName: "SKWingDown")
+        flapWing?.isHidden = true
     }
     
     func update(_ currentTime: TimeInterval) {
      
         position.y += yVelocity
         yVelocity -= 0.25
+        if position.y < -600 {
+            yVelocity = 0
+        }
+        if position.y > 620 {
+            yVelocity = 0
+            position.y = 620
+        }
         
-        
+        if yVelocity < 2.5 {
+            flapWing?.isHidden = true
+            upWing?.isHidden = false
+        } else {
+            flapWing?.isHidden = false
+            upWing?.isHidden = true
+        }
     }
     
     func didCollide(with body: SKPhysicsBody) {
@@ -30,5 +47,6 @@ class Hero: SKSpriteNode, GameObject {
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         yVelocity = 10
+        
     }
 }
