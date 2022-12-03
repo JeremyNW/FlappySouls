9b0942aad458f9469e764029822a281f9725bcc0
Jeremy Warren <jeddynwarren@gmail.com>
2022-12-02 23:06:39 -0700

    Google Ads

--- FlappySouls/GameViewController.swift
+++ FlappySouls/GameViewController.swift
@@ -8,9 +8,11 @@
 import UIKit
 import SpriteKit
 import GameplayKit
+import GoogleMobileAds
 
 class GameViewController: UIViewController {
     
+    @IBOutlet weak var bannerView: GADBannerView!
     @IBOutlet weak var adView: UIView!
     @IBOutlet weak var gameView: SKView!
     
@@ -20,6 +22,9 @@ class GameViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        bannerView.adUnitID = "ca-app-pub-3940256099942544/2934735716"
+        bannerView.rootViewController = self
+        bannerView.load(GADRequest())
         if let view = gameView {
             if let scene = SKScene(fileNamed: "MainScene") {
                 view.presentScene(scene)
