68fd2dfcc9d4861c09bc6a65631008a6b133791a
Jared Warren <wrrn24@gmail.com>
2022-12-04 01:12:56 -0700

    improved hero animation

--- FlappySouls/GameObjects/Hero.swift
+++ FlappySouls/GameObjects/Hero.swift
@@ -9,22 +9,29 @@ import Foundation
 import SpriteKit
 
 class Hero: SKSpriteNode, GameObject {
-    
+    let textures: [SKTexture] = [
+        SKTexture(imageNamed: "wing0"),
+        SKTexture(imageNamed: "wing1"),
+        SKTexture(imageNamed: "wing2"),
+        SKTexture(imageNamed: "wing3"),
+    ]
     var timer: Int = 15
     var controller: GameController!
     var yVelocity = CGFloat(0)
-    var upWings: [SKNode] = []
+    var upWings: [SKSpriteNode] = []
     var downWings: [SKNode] = []
+    var standardRotation = CGFloat(0)
     
     func setUp(with controller: GameController) {
         guard
-            let upZero = childNode(withName: "SKWingUp0"),
-            let upOne = childNode(withName: "SKWingUp1"),
+            let upZero = childNode(withName: "SKWingUp0") as? SKSpriteNode,
+            let upOne = childNode(withName: "SKWingUp1") as? SKSpriteNode,
             let downZero = childNode(withName: "SKWingDown0"),
             let downOne = childNode(withName: "SKWingDown1")
         else { return }
         upWings = [upZero, upOne]
         downWings = [downZero, downOne]
+        standardRotation = zRotation
         self.controller = controller
     }
     
@@ -40,9 +47,13 @@ class Hero: SKSpriteNode, GameObject {
             position.y = 620
         }
         
-        if yVelocity < 8.5 {
+        if yVelocity < 8 {
             downWings.forEach { $0.isHidden = true }
-            upWings.forEach { $0.isHidden = false }
+            let index = yVelocity < 6 ? 0 : yVelocity < 7 ? 3 : 2
+            for wing in upWings {
+                wing.isHidden = false
+                wing.texture = textures[index]
+            }
         } else {
             downWings.forEach { $0.isHidden = false }
             upWings.forEach { $0.isHidden = true }
@@ -57,8 +68,14 @@ class Hero: SKSpriteNode, GameObject {
             }
             timer = 15
         }
+        
+        if yVelocity <= -10 {
+            zRotation = standardRotation + yVelocity
+        } else {
+            zRotation = standardRotation
+        }
+        
         timer -= 1
-        print(self.position.x)
     }
     
     
